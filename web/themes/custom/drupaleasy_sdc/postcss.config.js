878a854634342d43d583517b1aba3d55cc15d27b
Michael Anello <michael@anelloconsulting.com>
2026-01-21 11:55:41 -0500

    End of class 2.

--- web/themes/custom/drupaleasy_sdc/postcss.config.js
+++ web/themes/custom/drupaleasy_sdc/postcss.config.js
@@ -21,11 +21,13 @@ const postcssImport = require('postcss-import');
 const postcssPxtorem = require('postcss-pxtorem');
 const postcssUrl = require('postcss-url');
 const postcssMixins = require('postcss-mixins');
+const postcssNested = require('postcss-nested');
 
 module.exports = {
   map: true,
   plugins: [
     postcssImport,
+    postcssNested,
     postcssPresetEnv,
     postcssMixins({ mixins }),
     postcssPxtorem({
